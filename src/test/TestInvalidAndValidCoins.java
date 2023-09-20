2ce2c3b612718bb4aa99347e60991f0a3115533e
Lasselarsen4 <10478730@ucn.dk>
2023-09-20 11:24:01 +0200

    Valid and invalid coin

---
+++ src/test/TestInvalidAndValidCoins.java
@@ -0,0 +1,56 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controllayer.ControlPayStation;
+import controllayer.IllegalCoinException;
+import modellayer.Currency;
+
+public class TestInvalidAndValidCoins {
+
+	ControlPayStation ps;
+
+	/** Fixture for pay station testing. */
+	@Before
+	public void setUp() {
+		ps = new ControlPayStation();
+	}
+	
+	@Test
+	public void shouldRejectIllegalCurrencyNokCoinAndAcceptDKK() throws IllegalCoinException {
+		// Arrange
+		boolean exceptionThrown = false;
+		int expectedParkingTime = 27;
+		int coinValue = 5;
+		Currency.ValidCurrency coinCurrency = Currency.ValidCurrency.NKK;
+		Currency.ValidCoinType coinType = Currency.ValidCoinType.INTEGER;
+		int coinValue2 = 5;
+		Currency.ValidCurrency coinCurrency2 = Currency.ValidCurrency.DKK;
+		Currency.ValidCoinType coinType2 = Currency.ValidCoinType.INTEGER;
+		//Act
+		try {
+			
+			ps.addPayment(coinValue, coinCurrency, coinType);
+			ps.addPayment(coinValue2, coinCurrency2, coinType2);
+
+
+		
+	} catch (IllegalCoinException e) {
+		exceptionThrown = true;
+	} finally {
+	ps.addPayment(coinValue2, coinCurrency2, coinType2);
+		
+	}
+		//Assert
+		assertTrue(exceptionThrown);
+		assertEquals(expectedParkingTime, ps.readDisplay());
+		
+		
+	    
+	}
+	
+}
